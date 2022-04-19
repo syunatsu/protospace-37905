11cc624e527fa8685628348d27facbe282dcd76a
syunatsu <myf.tyys@gmail.com>
2022-04-19 23:19:39 +0900

    prototypeコントローラーのshowアクションの修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -22,7 +22,7 @@ class PrototypesController < ApplicationController
   def show
     @prototype = Prototype.find(params[:id])
     @comment = Comment.new
-    @comments = @prototype.comments
+    @comments = @prototype.comments.includes(:user)
   end
 
   def edit
