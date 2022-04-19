ee2d9e6e1766a3b224d0c7f7c1ab10ee36f4f995
syunatsu <myf.tyys@gmail.com>
2022-04-19 19:58:07 +0900

    package.jsonの変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,4 +15,5 @@ class CommentsController < ApplicationController
     params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
   end
 
+  
 end
